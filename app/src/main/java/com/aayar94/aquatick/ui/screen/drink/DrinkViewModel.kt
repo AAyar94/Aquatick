d172bde6c3f7a8fdf7faaddd75ca4610fe24061e
Adem Ayar <a.ayar94@gmail.com>
2023-12-30 10:59:47 +0300

    drink view model init

---
+++ app/src/main/java/com/aayar94/aquatick/ui/screen/drink/DrinkViewModel.kt
@@ -0,0 +1,14 @@
+package com.aayar94.aquatick.ui.screen.drink
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class DrinkViewModel @Inject constructor(
+
+) : ViewModel() {
+
+
+    fun addDrink(){
+
+    }
+}
