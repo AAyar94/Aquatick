2d6581eab2878ea394eb8b3cce051a07fa7a7e2b
Adem Ayar <ademayar09@gmail.com>
2024-01-11 10:38:03 +0300

    export scheme added to db

--- app/src/main/java/com/aayar94/aquatick/data/local/user/UserDatabase.kt
+++ app/src/main/java/com/aayar94/aquatick/data/local/user/UserDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.aayar94.aquatick.data.model.user.UserDataModel
 
-@Database(entities = [UserDataModel::class], version = 1)
+@Database(entities = [UserDataModel::class], version = 1, exportSchema = false)
 abstract class UserDataDatabase : RoomDatabase() {
     abstract fun getUserDao(): UserDao
 }
