b6f8e8db4d97b0958c1bbd0a4ec5a3e597b526f5
Adem Ayar <ademayar09@gmail.com>
2024-01-16 00:45:16 +0300

    add Preferences

---
+++ core/src/main/java/com/aayar94/core/domain/preferences/DefaultPreferences.kt
@@ -0,0 +1,71 @@
+package com.aayar94.core.domain.preferences
+
+import android.content.SharedPreferences
+import com.aayar94.core.domain.model.ActivityLevel
+import com.aayar94.core.domain.model.Gender
+import com.aayar94.core.domain.model.UserInfo
+
+class DefaultPreferences(
+    private val sharedPreferences: SharedPreferences
+) : Preferences {
+    override fun saveName(name: String) {
+        sharedPreferences.edit()
+            .putString(Preferences.KEY_NAME, name)
+            .apply()
+    }
+
+    override fun saveGender(gender: Gender) {
+        sharedPreferences.edit()
+            .putString(Preferences.KEY_GENDER, gender.name)
+            .apply()
+    }
+
+    override fun saveAge(age: Int) {
+        sharedPreferences.edit()
+            .putInt(Preferences.KEY_AGE, age)
+            .apply()
+    }
+
+    override fun saveWeight(weight: Float) {
+        sharedPreferences.edit()
+            .putFloat(Preferences.KEY_WEIGHT, weight)
+            .apply()
+    }
+
+    override fun saveActivityLevel(activityLevel: ActivityLevel) {
+        sharedPreferences.edit()
+            .putString(Preferences.KEY_ACTIVITY_LEVEL, activityLevel.name)
+            .apply()
+    }
+
+    override fun saveDailyIntakeAmount(amount: Int) {
+        sharedPreferences.edit()
+            .putInt(Preferences.KEY_DAILY_INTAKE_AMOUNT, amount)
+            .apply()
+    }
+
+    override fun saveOnboardingFinishedState(state: Boolean) {
+        sharedPreferences.edit()
+            .putBoolean(Preferences.KEY_ONBOARDING_FINISHED_STATE, state)
+            .apply()
+    }
+
+    override fun getUserInfo(): UserInfo {
+        val name = sharedPreferences.getString(Preferences.KEY_NAME, "")
+        val age = sharedPreferences.getInt(Preferences.KEY_AGE, -1)
+        val weight = sharedPreferences.getFloat(Preferences.KEY_WEIGHT, -1f)
+        val genderString = sharedPreferences.getString(Preferences.KEY_GENDER, null)
+        val activityLevelString = sharedPreferences
+            .getString(Preferences.KEY_ACTIVITY_LEVEL, null)
+        val dailyIntakeAmount = sharedPreferences.getInt(Preferences.KEY_DAILY_INTAKE_AMOUNT, 0)
+
+        return UserInfo(
+            name = name!!,
+            gender = Gender.fromString(genderString!!),
+            age = age,
+            weight = weight,
+            activityLevel = ActivityLevel.fromString(activityLevelString!!),
+            dailyIntakeAmount = dailyIntakeAmount
+        )
+    }
+}
---
+++ core/src/main/java/com/aayar94/core/domain/preferences/Preferences.kt
@@ -0,0 +1,28 @@
+package com.aayar94.core.domain.preferences
+
+import com.aayar94.core.domain.model.ActivityLevel
+import com.aayar94.core.domain.model.Gender
+import com.aayar94.core.domain.model.UserInfo
+
+interface Preferences {
+    fun saveName(name: String)
+    fun saveGender(gender: Gender)
+    fun saveAge(age: Int)
+    fun saveWeight(weight: Float)
+    fun saveActivityLevel(activityLevel: ActivityLevel)
+    fun saveDailyIntakeAmount(amount: Int)
+
+    fun saveOnboardingFinishedState(state: Boolean)
+
+    fun getUserInfo(): UserInfo
+
+    companion object {
+        const val KEY_NAME = "key_name"
+        const val KEY_GENDER = "key_gender"
+        const val KEY_AGE = "key_age"
+        const val KEY_WEIGHT = "key_weight"
+        const val KEY_ACTIVITY_LEVEL = "key_activity_level"
+        const val KEY_DAILY_INTAKE_AMOUNT = "key_daily_intake_amount"
+        const val KEY_ONBOARDING_FINISHED_STATE = "key_onboarding_finished_state"
+    }
+}
