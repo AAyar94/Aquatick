8b9622bff45bdea1c2a04f9075cf07f1b542f339
Adem Ayar <ademayar09@gmail.com>
2024-01-16 16:29:04 +0300

    moved class for new project structure

--- onboarding/onboarding_presentation/src/main/java/com/aayar94/onboarding_presentation/component/StepItem.kt
+++ onboarding/onboarding_presentation/src/main/java/com/aayar94/onboarding_presentation/component/StepItem.kt
@@ -1,13 +1,13 @@
-package com.aayar94.aquatick.ui.component
+package com.aayar94.onboarding_presentation.component
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,5 +28,5 @@ fun StepItem(text: String, tint: Color) {
 @Preview
 @Composable
 fun PreviewStepItem() {
-    StepItem("Step One", MaterialTheme.colorScheme.primary)
+    StepItem("Step One", MaterialTheme.colors.primary)
 }
--- onboarding/onboarding_presentation/src/main/java/com/aayar94/onboarding_presentation/component/WorkInProgress.kt
+++ onboarding/onboarding_presentation/src/main/java/com/aayar94/onboarding_presentation/component/WorkInProgress.kt
@@ -1,4 +1,4 @@
-package com.aayar94.aquatick.ui.component
+package com.aayar94.onboarding_presentation.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -7,8 +7,8 @@ import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -16,8 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.aayar94.aquatick.R
-import com.aayar94.aquatick.util.DevicesPreview
+import com.aayar94.core_ui.util.DevicesPreview
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.rememberLottieComposition
@@ -28,7 +27,7 @@ fun WorkInProgress(where: String) {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background),
+            .background(MaterialTheme.colors.background),
         contentAlignment = Alignment.Center
     ) {
         Column(
@@ -37,15 +36,19 @@ fun WorkInProgress(where: String) {
                 .padding(horizontal = 12.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.wip))
+            val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(com.aayar94.core.R.raw.wip))
             LottieAnimation(
                 composition = composition,
                 modifier = Modifier.defaultMinSize(64.dp, 64.dp),
                 iterations = 5
             )
             Text(
-                text = stringResource(R.string.work_in_progress_in_this_screen_screen, where),
-                color = MaterialTheme.colorScheme.onBackground, textAlign = TextAlign.Center
+                text = stringResource(
+                    com.aayar94.core.R.string.work_in_progress_in_this_screen_screen,
+                    where
+                ),
+                color = MaterialTheme.colors.onBackground,
+                textAlign = TextAlign.Center
             )
         }
     }
