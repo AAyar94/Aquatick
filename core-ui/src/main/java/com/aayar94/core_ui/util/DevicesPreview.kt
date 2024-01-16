2ef15f224a3243e93c3fa7439697b641c9ba6052
Adem Ayar <ademayar09@gmail.com>
2024-01-16 08:03:08 +0300

    renamed DevicesPreview annotation class

---
+++ core-ui/src/main/java/com/aayar94/core_ui/util/DevicesPreview.kt
@@ -0,0 +1,9 @@
+package com.aayar94.core_ui.util
+
+import android.content.res.Configuration
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(name = "Pixel4Day", device = Devices.PIXEL_4_XL)
+@Preview(name = "Pixel4Night", device = Devices.PIXEL_4_XL, uiMode = Configuration.UI_MODE_NIGHT_YES)
+annotation class DevicesPreview
