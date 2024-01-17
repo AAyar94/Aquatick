8856e55a4a068af12db49c31f61c5bab03a3201c
Adem Ayar <ademayar09@gmail.com>
2024-01-17 23:28:43 +0300

    add DrinkScreen, add SettingsScreen

---
+++ aquatracker/aquatracker_presentation/src/main/java/com/aayar94/aquatracker_presentation/drink/DrinkScreen.kt
@@ -0,0 +1,9 @@
+package com.aayar94.aquatracker_presentation.drink
+
+import androidx.compose.runtime.Composable
+import com.aayar94.core_ui.util.WorkInProgress
+
+@Composable
+fun DrinkScreen() {
+        WorkInProgress(whatIsThisRoute = "DrinkScreen")
+}
---
+++ settings/settings_presentation/src/main/java/com/aayar94/settings_presentation/SettingsScreen.kt
@@ -0,0 +1,9 @@
+package com.aayar94.settings_presentation
+
+import androidx.compose.runtime.Composable
+import com.aayar94.core_ui.util.WorkInProgress
+
+@Composable
+fun SettingsScreen() {
+    WorkInProgress("SettingsScreen")
+}
