ec1280f4ba76a845a3ee2a69858f77b40ff81ef2
Adem Ayar <ademayar09@gmail.com>
2024-01-20 16:05:39 +0300

    bug fix composable preview not working

--- core-ui/build.gradle.kts
+++ core-ui/build.gradle.kts
@@ -8,3 +8,8 @@ apply(from = "$rootDir/compose-module.gradle")
 android {
     namespace = "com.aayar94.core_ui"
 }
+
+dependencies{
+    debugImplementation( "androidx.compose.ui:ui-tooling:1.5.4")
+    implementation ("androidx.compose.ui:ui-tooling-preview:1.5.4")
+}
