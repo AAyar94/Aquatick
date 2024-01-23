607f636b0eb7b8628aae560352b60795bc2bb23a
Adem Ayar <ademayar09@gmail.com>
2024-01-23 14:03:58 +0300

    bug fix for small screen , screen overflowing

--- onboarding/onboarding_presentation/src/main/java/com/aayar94/onboarding_presentation/activity_level/ActivityLevelScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/aayar94/onboarding_presentation/activity_level/ActivityLevelScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -31,6 +33,7 @@ fun ActivityLevelScreen(
 ) {
     val spacing = LocalSpacing.current
     val shapes = LocalShape.current
+    val scrollState = rememberScrollState()
     LaunchedEffect(true) {
         viewModel.uiEvent.collect { event ->
             when (event) {
@@ -48,7 +51,7 @@ fun ActivityLevelScreen(
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(spacing.spaceMedium),
+                .padding(spacing.spaceMedium).verticalScroll(scrollState),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
