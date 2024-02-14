2c1e4c8c3eaebe1c4387336d66ad002dc8e1485c
Adem Ayar <ademayar09@gmail.com>
2024-02-14 23:38:55 +0300

    fix: Delete annotation crashed cause by room - kotlin version compatibility

--- aquatracker/aquatracker_data/src/main/java/com/aayar94/aquatracker_data/local/DrinkDao.kt
+++ aquatracker/aquatracker_data/src/main/java/com/aayar94/aquatracker_data/local/DrinkDao.kt
@@ -6,7 +6,6 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.aayar94.aquatracker_data.local.entity.DrinkEntity
-import com.aayar94.aquatracker_domain.model.ChartModel
 import com.aayar94.aquatracker_domain.model.DayTotalAmount
 import kotlinx.coroutines.flow.Flow
 
@@ -29,22 +28,31 @@ interface DrinkDao {
     fun getDrinksForDate(day: Int, month: Int, year: Int): Flow<List<DrinkEntity>>
 
     @Query("SELECT * FROM drinks_table ORDER BY ID DESC LIMIT 3")
-    fun getLast3Object() : Flow<List<DrinkEntity>>
+    fun getLast3Object(): Flow<List<DrinkEntity>>
 
     @Query("DELETE FROM drinks_table")
     suspend fun deleteDatabase()
 
-    @Query("SELECT * FROM drinks_table WHERE year = :year" +
-            " AND month = :month " +
-            "AND dayOfMonth BETWEEN :startDay AND :endDay " +
-            "ORDER BY year DESC, month DESC, dayOfMonth DESC, hour DESC, minute DESC, second DESC")
+    @Query(
+        "SELECT * FROM drinks_table WHERE year = :year" +
+                " AND month = :month " +
+                "AND dayOfMonth BETWEEN :startDay AND :endDay " +
+                "ORDER BY year DESC, month DESC, dayOfMonth DESC, hour DESC, minute DESC, second DESC"
+    )
     suspend fun getDrinksForSevenDays(
-        startDay:Int,
-        endDay:Int,
-        month:Int,
-        year:Int
-    ):List<DrinkEntity>
+        startDay: Int,
+        endDay: Int,
+        month: Int,
+        year: Int
+    ): List<DrinkEntity>
 
     @Query("SELECT dayOfMonth, SUM(drinkAmount) as totalAmount FROM drinks_table WHERE (year = :year AND month = :month AND dayOfMonth >= :startDay) OR (year = :prevYear AND month = :prevMonth AND dayOfMonth >= :prevStartDay) GROUP BY dayOfMonth ORDER BY dayOfMonth DESC LIMIT 7")
-    suspend fun getLast7DaysTotalAmount(year: Int, month: Int, startDay: Int, prevYear: Int, prevMonth: Int, prevStartDay: Int): List<DayTotalAmount>
+    suspend fun getLast7DaysTotalAmount(
+        year: Int,
+        month: Int,
+        startDay: Int,
+        prevYear: Int,
+        prevMonth: Int,
+        prevStartDay: Int
+    ): List<DayTotalAmount>
 }
