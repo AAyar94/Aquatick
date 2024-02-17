99dd745b07d96b4ed6d588a25f82fb731859a675
Adem Ayar <ademayar09@gmail.com>
2024-02-18 00:00:10 +0300

    removed: DailyScreen.kt, DailyScreenViewModel.kt, DailyUIState.kt

--- aquatracker/aquatracker_presentation/src/main/java/com/aayar94/aquatracker_presentation/analysis/sub_screen/daily/DailyUIState.kt
+++
@@ -1,9 +0,0 @@
-package com.aayar94.aquatracker_presentation.analysis.sub_screen.daily
-
-import com.patrykandpatrick.vico.core.entry.ChartEntryModel
-
-data class DailyUIState(
-    val isLoading: Boolean = true,
-    val list: ChartEntryModel? = null,
-    val defaultDailyIntakeAmount: Int? = null
-)
