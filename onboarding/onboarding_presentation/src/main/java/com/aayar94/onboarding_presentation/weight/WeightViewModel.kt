80b919f70d79fa83c947bc18afbd89df9042243f
Adem Ayar <ademayar09@gmail.com>
2024-02-18 18:02:33 +0300

    fix: WeightViewModel can't handle weight text states

--- onboarding/onboarding_presentation/src/main/java/com/aayar94/onboarding_presentation/weight/WeightViewModel.kt
+++ onboarding/onboarding_presentation/src/main/java/com/aayar94/onboarding_presentation/weight/WeightViewModel.kt
@@ -1,7 +1,7 @@
 package com.aayar94.onboarding_presentation.weight
 
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -18,30 +18,29 @@ class WeightViewModel @Inject constructor(
     private val preferences: com.aayar94.core.domain.preferences.Preferences
 ) : ViewModel() {
 
-    var weightState by mutableFloatStateOf(80.0f)
+    var weightState by mutableStateOf("80.0")
         private set
 
     private val _uiEvent = Channel<UiEvent>()
     val uiEvent = _uiEvent.receiveAsFlow()
 
     fun weightChange(weight: Float) {
-        weightState = weight
+        weightState = weight.toString()
     }
 
     fun onNextClick() {
         viewModelScope.launch {
-            kotlin.run {
-                if (weightState == 0f) {
-                    _uiEvent.send(
-                        UiEvent.ShowSnackbar(
-                            UiText.StringResource(com.aayar94.core.R.string.weight_cannot_be_empty)
-                        )
+            if (weightState.isBlank()) {
+                _uiEvent.send(
+                    UiEvent.ShowSnackbar(
+                        UiText.StringResource(com.aayar94.core.R.string.weight_cannot_be_empty)
                     )
-                    return@run
-                }
-                preferences.saveWeight(weightState)
+                )
+            } else {
+                preferences.saveWeight(weightState.toFloat())
                 _uiEvent.send(UiEvent.Success)
             }
         }
     }
+
 }
