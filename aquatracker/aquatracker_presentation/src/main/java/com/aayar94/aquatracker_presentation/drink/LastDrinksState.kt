16185f075dc42e925a72cb42a895975eb6f1cd0b
Adem Ayar <ademayar09@gmail.com>
2024-02-23 20:28:04 +0300

    delete: unused state class

--- aquatracker/aquatracker_presentation/src/main/java/com/aayar94/aquatracker_presentation/drink/LastDrinksState.kt
+++
@@ -1,7 +0,0 @@
-package com.aayar94.aquatracker_presentation.drink
-
-import com.aayar94.aquatracker_domain.usecase.ScreenDrinkItem
-
-data class LastDrinksState(
-    var list: List<ScreenDrinkItem> = emptyList()
-)
